682c70cc4886c349e60351eee34384b56a5fc3ea
Prerna611 <ps546866@gmail.com>
2018-07-08 17:04:48 +0530

    hi

--- Game_script.js
+++ Game_script.js
@@ -1,31 +1,38 @@
-var colors=[
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)"
+var colors=generaterandomcolor(6);
 
-]
+
 var squares = document.querySelectorAll(".square");
-
-var colorpicked=colors[3];
+var resetbutton=document.querySelector("#reset");
+var colorpicked=pickcolors();
 var colordisplay=document.getElementById("colordisplay");
 var message=document.querySelector("#message"); 
+var h1=document.querySelector("h1");
+
+resetbutton.addEventListener("click",function(){
+colors=generaterandomcolor(6);
+colorpicked=pickcolors();
+colordisplay.textContent=colorpicked;	
+for(var i=0;i<squares.length;i++)
+	{
+	squares[i].style.backgroundColor=colors[i];
+}
+})
 colordisplay.textContent = colorpicked;
+
 for(var i = 0; i < squares.length; i++)
 {
 	squares[i].style.backgroundColor = colors[i];
 	squares[i].addEventListener("click",function(){
 		var clickedcolor = this.style.backgroundColor;
-
+console.log(clickedcolor,colorpicked);
 if(clickedcolor === colorpicked)
 {
 	message.textContent="correct";
 	for(var i=0;i<squares.length;i++)
 	{
-	squares[i].style.backgroundColor=clickedcolor;
+	squares[i].style.backgroundColor=colorpicked;
 }
+h1.style.backgroundColor=colorpicked;
 }
 else
 {
@@ -34,4 +41,24 @@ else
 }
 	});
 }
-
+function pickcolors()
+{
+	var m=Math.floor(Math.random()*colors.length);
+	return colors[m];
+}
+function generaterandomcolor(num)
+{
+var arr = []
+for(var i=0;i<num;i++)
+{
+	arr.push(randomcolors())
+}
+return arr;
+}
+function randomcolors()
+{
+	var r=Math.floor(Math.random()*256);
+	var g=Math.floor(Math.random()*256);
+	var b=Math.floor(Math.random()*256);
+	return "rgb("+ r +", "+ g +", "+b +")";
+}
